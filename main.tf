478cc70a896d7bb7796a867ab14f5826f3f2d189
cxuanyi-w <cxuanyi.w@gmail.com>
2022-05-29 13:36:02 +0800

    can create EC2 with pem uploaded

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-variable "x" {
-  default="4"
-}
-
 resource "tls_private_key" "example" {
   algorithm = "RSA"
   rsa_bits  = 4096
@@ -12,15 +8,32 @@ resource "aws_key_pair" "generated_key" {
   public_key = tls_private_key.example.public_key_openssh
 }
 
-resource "local_file" "create-private-key-file" {
+resource "local_file" "private-key-file" {
   filename = "${path.module}/ec2-key-terraform-${var.x}.pem"
   content  = tls_private_key.example.private_key_pem
 }
 
 //security.tf
-resource "aws_security_group" "ingress-all-test" {
-  name   = "allow-all-sg"
-  vpc_id = "vpc-04fa1d91cea29daa0"
+resource "aws_security_group" "public-ec2-sg" {
+  name   = "allow-all-public-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+  } // Terraform removes the default rule
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 8
+    to_port     = 0
+    protocol    = "icmp"
+    description = "Allow ping from 1.2.3.4"
+  }
 
   ingress {
     cidr_blocks = [
@@ -38,22 +51,105 @@ resource "aws_security_group" "ingress-all-test" {
   }
 }
 
-resource "aws_instance" "create-temp-ec2" {
-  ami                         = "ami-0745bc24bc4178882"
+resource "aws_security_group" "private-ec2-sg" {
+  name   = "allow-ping-private-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    cidr_blocks = ["192.168.0.0/24"]
+    from_port   = 8
+    to_port     = 0
+    protocol    = "icmp"
+    description = "allow ping"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "public-ec2" {
+  ami                         = "ami-04d9e855d716f9c99"
   instance_type               = "t2.micro"
-  subnet_id                   = "subnet-088a8e16a499a5799"
+  subnet_id                   = var.public_subnet_id
   associate_public_ip_address = true
-  security_groups             = ["${aws_security_group.ingress-all-test.id}"]
-  
+  security_groups = [
+    "${aws_security_group.public-ec2-sg.id}",
+    "${aws_security_group.public-ec2-sg.id}"
+  ]
+
   key_name = "ec2-key-terraform-${var.x}"
   #   --instance-initiated-shutdown-behavior terminate
 
- tags = {
-    Name = "terroform-created-${var.x}"
+  provisioner "local-exec" {
+    command = "cd ${path.module}"
+  }
+
+  provisioner "local-exec" {
+    command = "rm -f ${path.module}/ec2-key-terraform-${var.x}-temp.pem"
+  }
+
+  provisioner "local-exec" {
+    command = "cp ${path.module}/ec2-key-terraform-${var.x}.pem ${path.module}/ec2-key-terraform-${var.x}-temp.pem"
+  }
+
+  provisioner "local-exec" {
+    command = "chmod 400 ${path.module}/ec2-key-terraform-${var.x}.pem"
+  }
+
+  provisioner "file" {
+    source      = "${path.module}/ec2-key-terraform-${var.x}.pem"
+    destination = "/home/ubuntu/ec2-key-terraform-${var.x}.pem"
+
+    connection {
+      host        = self.public_ip
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = tls_private_key.example.private_key_pem
+    }
+  }
+
+  depends_on = [local_file.private-key-file]
+
+  tags = {
+    Name = "cxy-terraform-vpc-public-ec2-${var.x}"
+  }
+}
+
+resource "aws_instance" "private-ec2-1" {
+  ami           = "ami-04d9e855d716f9c99"
+  instance_type = "t2.micro"
+  subnet_id     = var.private_subnet_id_1
+  # associate_public_ip_address = true
+  security_groups = ["${aws_security_group.private-ec2-sg.id}"]
+
+  key_name = "ec2-key-terraform-${var.x}"
+  #   --instance-initiated-shutdown-behavior terminate
+
+  tags = {
+    Name = "cxy-terraform-vpc-private-ec2-${var.x}"
+  }
+}
+
+resource "aws_instance" "private-ec2-2" {
+  ami           = "ami-04d9e855d716f9c99"
+  instance_type = "t2.micro"
+  subnet_id     = var.private_subnet_id_2
+  # associate_public_ip_address = true
+  security_groups = ["${aws_security_group.private-ec2-sg.id}"]
+
+  key_name = "ec2-key-terraform-${var.x}"
+  #   --instance-initiated-shutdown-behavior terminate
+
+  tags = {
+    Name = "cxy-terraform-vpc-private-ec2-${var.x}"
   }
 }
 
 resource "aws_eip" "ip-test-env" {
-  instance = "${aws_instance.create-temp-ec2.id}"
+  instance = aws_instance.public-ec2.id
   vpc      = true
 }
--- variables.tf
+++ variables.tf
@@ -1,2 +1,22 @@
 # Provider variables
 variable "region" {}
+
+variable "x" {
+  default="4"
+}
+
+variable "vpc_id" {
+  default="vpc-00e35cec217b5d441"
+}
+
+variable "public_subnet_id" {
+  default="subnet-0385835828df121eb"
+}
+
+variable "private_subnet_id_1" {
+  default="subnet-06f6ab1705a51042d"
+}
+
+variable "private_subnet_id_2" {
+  default="subnet-09dd42e5c589ea1d2"
+}
